1487a8252552a4325d5da59119e99688260ef30c
alonso.torres <alonso.torres@kaleidos.net>
2014-05-26 15:27:29 +0200

    US #59 Plugin cleanup

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,23 +1,6 @@
-// configuration for plugin testing - will not be included in the plugin zip
-
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+    error  'org.codehaus.groovy.grails',
            'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+           'org.hibernate'
 }
 grails.doc.images = new File("src/docs/images")
