ab3afd4fc13123756a46186b63c898c889677033
Juanfran <juanfran.alcantara@kaleidos.net>
2014-06-06 10:36:31 +0200

    map widget js refactor

--- web-app/js/admin/mapwidget.js
+++ web-app/js/admin/mapwidget.js
@@ -2,9 +2,20 @@ app.view('mapwidget', ['$el'], function ($el) {
     "use strict";
 
     $el.on( "click", ".js-map-widget-refresh", function(event) {
-        var value = $(this).closest(".map-widget").find(".js-map-widget-text").val();
-        var html = "<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'";
-        html += "src='https://maps.google.com/maps?f=q&amp;q=" + value + "&amp;output=embed'></iframe>";
-        $(this).closest(".map-widget").find(".map-container").html(html);
+        var value = $el.find(".js-map-widget-text").val();
+        var mapContainer = $el.find(".map-container");
+
+        var mapIframe = $("<iframe>")
+            .attr({
+                "width": 425,
+                "height": 350,
+                "frameborder": 0,
+                "scrolling": 0,
+                "marginheight": 0,
+                "marginwidth": 0,
+                "src": "https://maps.google.com/maps?f=q&q=" + value + "&output=embed"
+            });
+
+        mapContainer.html(mapIframe)
     });
 });
