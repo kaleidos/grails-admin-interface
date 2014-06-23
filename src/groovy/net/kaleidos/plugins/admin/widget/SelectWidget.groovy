87d618e1eb8a6b82275886ff23102e12cd88e40d
Jesús Espino <jesus.espino@kaleidos.net>
2014-06-23 08:25:33 +0200

    Converting all widgets to groovy.xml.MarkupBuilder

--- src/groovy/net/kaleidos/plugins/admin/widget/SelectWidget.groovy
+++ src/groovy/net/kaleidos/plugins/admin/widget/SelectWidget.groovy
@@ -1,41 +1,37 @@
 package net.kaleidos.plugins.admin.widget
 
+import groovy.xml.MarkupBuilder
+
 class SelectWidget extends Widget {
 
     @Override
     String render() {
-        StringBuilder html = new StringBuilder()
+        def writer = new StringWriter()
+        def builder = new MarkupBuilder(writer)
 
-        html.append("<select")
-        htmlAttrs.each {key, value ->
-            html.append(" ${key.encodeAsHTML()}=\"${value.encodeAsHTML()}\"")
-        }
-        html.append(">")
+        builder.select(htmlAttrs) {
+            if (!htmlAttrs['required']) {
+                option(value:"", "--")
+            }
 
-        if (!htmlAttrs['required']) {
-            html.append("<option value=\"\">--</option>")
-        }
-
-        // draw options values
-        if (internalAttrs.options) {
-            internalAttrs.options.each {val, text ->
-                html.append("<option value=\"${val.encodeAsHTML()}\"")
-                if ((val as String) == (value as String)) {
-                    html.append(" selected=\"selected\"")
+            if (internalAttrs.options) {
+                internalAttrs.options.each {val, text ->
+                    def optionAttrs = ["value": val]
+                    if ((val as String) == (value as String)) {
+                        optionAttrs << ["selected": "selected"]
+                    }
+                    option(optionAttrs, text)
                 }
-                html.append(">${text.encodeAsHTML()}</option>")
             }
         }
-
-        html.append("</select>")
-        return html
+        return writer
     }
 
     List<String> getAssets() {
         def results = [
-            'libs/select2/select2.css',
-            'libs/select2/select2-bootstrap.css',
-            'libs/select2/select2.js'
+            'grails-admin/libs/select2/select2.css',
+            'grails-admin/libs/select2/select2-bootstrap.css',
+            'grails-admin/libs/select2/select2.js'
         ]
         return results.collect { ["plugin":"admin-interface", "absolute":true, "file":it]  }
     }
